9b170b627b8dd84d5f6e1fc4978435c02ad40912
okarthikb <86470305+okarthikb@users.noreply.github.com>
2023-03-21 01:11:11 -0400

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -3,69 +3,56 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import einsum
 from einops import rearrange, repeat
-
-
-class SwiGLU(nn.Module):
-  def forward(self, x):
-    x, g = x.chunk(2, dim=-1)
-    return x * F.silu(g)
+from torch.optim.lr_scheduler import _LRScheduler
 
 
 class RMSNorm(nn.Module):
-  def __init__(self, d):
+  def __init__(self, d, eps=1e-8):
     super().__init__()
-    assert isinstance(d, int), 'd must be an int'
-    self.g = nn.Parameter(torch.ones(d))
+    self.eps = eps
+    self.weight = nn.Parameter(torch.ones(d))
 
   def forward(self, x):
-    assert x.shape[-1] == self.d, f'd must be {self.d}'
-    return self.g * x * torch.rsqrt((x * x).mean(-1, keepdim=True) + 1e-6)
+    rrms = torch.rsqrt((x * x).mean(-1, keepdim=True) + self.eps) 
+    return self.weight * x * rrms
 
 
 class RotaryEmbedding(nn.Module):
   def __init__(self, l, d):
     super().__init__()
-    assert d % 2 == 0, f'd must be even'
-    self.d, self.l = d, l
-    θ = 1e4 ** (repeat(-torch.arange(0, d, 2), 'i -> (i j)', j=2) / d)
+    θ = 1e4 ** (-repeat(torch.arange(0, d, 2), 'i -> (i j)', j=2) / d)
     lθ = einsum('i, j -> ij', torch.arange(l), θ)
     self.sin = nn.Parameter(lθ.sin(), requires_grad=False)
     self.cos = nn.Parameter(lθ.cos(), requires_grad=False)
 
   def forward(self, x):
-    _, l, d = x.shape
-    flip = torch.cat((-x[..., 1::2, None], x[..., ::2, None]), -1)
-    x_ = rearrange(flip, 'b l x y -> b l (x y)')
-    return x * self.cos[:l] + x_ * self.sin[:l]
+    _, l, _ = x.shape
+    y2 = torch.cat((-x[..., 1::2, None], x[..., ::2, None]), -1)
+    y = rearrange(y2, '... x y -> ... (x y)')
+    return x * self.cos[:l] + y * self.sin[:l]
 
 
 class Layer(nn.Module):
-  def __init__(self, d, nh):
+  def __init__(self, d, nh, pe=lambda x: x):
     super().__init__()
-    assert d % nh == 0, 'nh must divide d'
-    self.d, self.c = d, d ** -0.5
+    self.scale = d ** -0.5
+    self.wx, self.wo = nn.Linear(d, 3 * d), nn.Linear(d, d) 
+    self.mhanorm, self.ffnorm = RMSNorm(d), RMSNorm(d)
+    self.pe = pe
+    self.split = lambda x: rearrange(x, 'b l (nh dh) -> b nh l dh', nh=nh)
 
-    self.wx = nn.Linear(d, 3 * d, bias=False)
-    self.wo = nn.Linear(d, d, bias=False)
-    self.norm1, self.norm2 = RMSNorm(d), RMSNorm(d)
-    self.ff = nn.Sequential(nn.Linear(d, 6 * d), SwiGLU(), nn.Linear(3 * d, d))
-
-    self.rotate = lambda x: x
-
-    self.split = lambda x: rearrange(
-      rearrange(x, 'b l d -> b d l'), 'b (nh dh) l -> b nh dh l', nh=nh
-    )
-    self.concat = lambda x: rearrange(
-      rearrange(x, 'b nh dh l -> b (nh dh) l'), 'b d l -> b l d'
-    )
+    h = 8 * d // 3
+    self.u, self.v = nn.Linear(d, h, bias=False), nn.Linear(d, h, bias=False)
+    self.w = nn.Linear(h, d, bias=False)
+    self.ff = lambda x: self.w(F.silu(self.u(x)) * self.v(x))
 
   def forward(self, xm):
     x, m = xm
-    q, k, v = self.wx(self.norm1(x)).split(self.d, -1)
-    q, k, v = map(self.split, (self.rotate(q), self.rotate(k), v))
-    A = F.softmax((einsum('bhri, bhrj -> bhij', q, k) + m) * self.c, -1)
-    x = x + self.wo(self.concat(einsum('bhic, bhjc -> bhij', v, A)))
-    return x + self.ff(self.norm2(x)), m
+    q, k, v = rearrange(self.wx(self.mhanorm(x)), 'b l (n d) -> n b l d', n=3)
+    q, k, v = map(self.split, (self.pe(q), self.pe(k), v))
+    A = F.softmax(einsum('bhic, bhjc -> bhij', q, k) * self.scale + m, -1)
+    x = x + self.wo(rearrange(A @ v, 'b nh l dh -> b l (nh dh)'))
+    return x + self.ff(self.ffnorm(x)), m
 
 
 class GPT(nn.Module):
@@ -76,11 +63,8 @@ class GPT(nn.Module):
     self.emb = nn.Embedding(v, d)
     self.out = nn.Linear(d, v, bias=False)
 
-    self.rotate = RotaryEmbedding(l, d)
-
-    self.layers = nn.Sequential(*[Layer(d, nh) for _ in range(nl)])
-    for layer in self.layers:
-      layer.rotate = self.rotate
+    self.rope = RotaryEmbedding(l, d)
+    self.layers = nn.Sequential(*[Layer(d, nh, self.rope) for _ in range(nl)])
 
     m = torch.tril(torch.ones(l, l)) - 1
     m[m == -1] = float('-inf')
@@ -95,15 +79,65 @@ class GPT(nn.Module):
     self.apply(fn)
     nn.init.normal_(self.emb.weight, 0, 0.02)
 
-  def forward(self, T):
-    assert len(T.shape) == 2, 'input must have 2 dimensions'
-    _, l = T.shape
-    assert l <= self.l, f'length must be <= {self.l}'
-    xm = (self.emb(T), self.m[:l, :l])
-    x, _ = self.layers(xm)
+    self.np = sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+  def param_groups(self, weight_decay=1e-2):
+    decay, nodecay = set(), set()
+
+    for mn, m in self.named_modules():
+      for pn, p in m.named_parameters():
+        fpn = f'{mn}.{pn}' if mn else pn
+        if pn.endswith('bias'):
+          nodecay.add(fpn)
+        elif pn.endswith('weight') and isinstance(m, nn.Linear):
+          decay.add(fpn)
+        elif pn.endswith('weight') and isinstance(m, (nn.Embedding, RMSNorm)):
+          nodecay.add(fpn)
+   
+    pdict = {pn: p for pn, p in self.named_parameters() if p.requires_grad}
+    assert len(decay & nodecay) == 0, f'{decay & nodecay}'
+    assert len(pdict.keys() - (decay | nodecay)) == 0
+    decay = [pdict[pn] for pn in sorted(list(decay))]
+    nodecay = [pdict[pn] for pn in sorted(list(nodecay))]
+
+    return [
+      {'params': decay, 'weight_decay': weight_decay},
+      {'params': nodecay, 'weight_decay': 0}
+    ]
+
+  def forward(self, x):
+    _, l = x.shape 
+    x, _ = self.layers((self.emb(x), self.m[:l, :l]))
     return self.out(x)
 
-  def loss(self, Tx, Ty):
+  def loss(self, x, y):
     return F.cross_entropy(
-      rearrange(self(Tx), 'b l v -> (b l) v'), rearrange(Ty, 'b l -> (b l)')
+      rearrange(self(x), 'b l v -> (b l) v'), rearrange(y, 'b l -> (b l)')
     )
+
+
+class InvSqrtLR(_LRScheduler):
+  def __init__(self, optimizer, max_lr, min_lr, warmup, steps):
+    self.optimizer, self.steps = optimizer, steps
+
+    d = steps ** 0.5 - warmup ** 0.5
+    scale = (max_lr - min_lr) * (steps * warmup) ** 0.5 / d
+    shift = (min_lr * steps ** 0.5 - max_lr * warmup ** 0.5) / d
+    
+    linear = lambda x: max_lr * x / warmup
+    invsqrt = lambda x: scale * (1 / x) ** 0.5 + shift
+    self.schedule = lambda x: min(linear(x), invsqrt(x))
+    
+    self.cur_step = 0
+    super().__init__(optimizer) 
+ 
+  def get_lr(self):
+    return [group['lr'] for group in self.optimizer.param_groups]
+  
+  def step(self):
+    assert self.cur_step <= self.steps, 'cannot exceed max steps'
+    self.cur_step += 1
+    lr = self.schedule(self.cur_step)
+    for group in self.optimizer.param_groups:
+      group['lr'] = lr
+    super().step(None)
